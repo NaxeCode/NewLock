c3ab728b831395634470887539427202058a6af5
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-07-20 19:05:03 -0400

    Updated layout, removed description, added EZ number in description

--- NewLock/Services/ProductRepository.cs
+++ NewLock/Services/ProductRepository.cs
@@ -9,11 +9,17 @@ public class ProductRepository
     public ProductRepository()
     {
         string lever_description = @"The 800-Series locks are designed for use in a variety of commercial applications. Equipped with a UL listed latchbolt, these locks can be used on 3-hour fire doors. Built with independent return springs in each lever and with an optional clutch feature to protect against excessive torque, these levers resist sagging. The L-shaped lever design with less than 1/2"" gap between the handle and the door meets most accessibility codes. Compatible with many retrofit cylinders such as LSDA C500, Ilco, GMS and most High-Security brands including Medeco, Assa, Schlage Primus, Kaba Peaks, etc.";
-
+        string short_desc = "Just testing";
         _products = new List<Product>
         {
             new Product { Id = "1", Ez = "181301" , Name = "Grade 2 Entry Lever", Price = 189.99, Description = lever_description },
-            new Product { Id = "2", Ez = "181305" , Name = "Grade 2 Storeroom Lever", Price = 189.99, Description = lever_description}
+            new Product { Id = "2", Ez = "181305" , Name = "Grade 2 Storeroom Lever", Price = 189.99, Description = short_desc},
+            new Product { Id = "2", Ez = "181305" , Name = "Grade 2 Storeroom Lever", Price = 189.99, Description = short_desc},
+            new Product { Id = "2", Ez = "181305" , Name = "Grade 2 Storeroom Lever", Price = 189.99, Description = short_desc},
+            new Product { Id = "2", Ez = "181305" , Name = "Grade 2 Storeroom Lever", Price = 189.99, Description = short_desc},
+            new Product { Id = "2", Ez = "181305" , Name = "Grade 2 Storeroom Lever", Price = 189.99, Description = short_desc},
+            new Product { Id = "2", Ez = "181305" , Name = "Grade 2 Storeroom Lever", Price = 189.99, Description = short_desc},
+            new Product { Id = "2", Ez = "181305" , Name = "Grade 2 Storeroom Lever", Price = 189.99, Description = short_desc}
         };
     }
 
