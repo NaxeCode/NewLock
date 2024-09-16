8d168be674bace196ee2b6e15b76326cf7c347ca
“NaxeCode” <15963333+NaxeCode@users.noreply.github.com>
2024-09-15 23:29:00 -0400

    Ported structure from MVC app things are broken

---
+++ LockSite/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NewLock.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ LockSite/Models/Product.cs
@@ -0,0 +1,13 @@
+namespace NewLock.Models;
+
+public class Product
+{
+    public string Id { get; set; }
+    public string Ez { get; set; }
+
+    public string Name { get; set; }
+
+    public double Price { get; set; }
+
+    public string Description { get; set; }
+}
